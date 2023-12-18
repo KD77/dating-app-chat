f82bc9a2f8bad9a8104dc4ed2cd590f424a71317
KD77 <kalid.diriye@gmail.com>
2023-12-18 03:12:54 +0100

    fix chat history

--- controllers/ChatController.js
+++ controllers/ChatController.js
@@ -1,52 +1,92 @@
 const express = require('express');
 const Chat = require('../model/Chat');
 
-exports.getChatHistory = async (req, res) => {
+
+const getChatHistory = async (req, res) => {
   try {
     const userId = req.params.userId;
-    // Query the database to get chat history for the specified user ID
-    const chatHistory = await Chat.find({ receiver: userId }).sort({ createdAt: 1 }).exec();
-    res.json(chatHistory);
+
+    // Retrieve chat history for the current user
+    const currentUserChatHistory = await getChatHistoryForCurrentUser(userId);
+
+    // Retrieve chat history for the selected receiver
+    const selectedReceiverChatHistory = await getChatHistoryForSelectedReceiver(userId);
+
+    // Combine the chat histories of both users
+    const combinedChatHistory = currentUserChatHistory.concat(selectedReceiverChatHistory);
+
+    // Sort the combined chat history by createdAt
+    combinedChatHistory.sort((a, b) => b.createdAt - a.createdAt);
+
+    // Send the combined chat history
+    res.json(combinedChatHistory);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server error', error: error.message });
+    res.status(500).json({ message: 'Server error,', error: error.message });
   }
 };
 
-exports.send = async (req, res) => {
+const getChatHistoryForCurrentUser = async (userId) => {
+  const chatHistoryForCurrentUser = await Chat.find({
+    receiver: userId,
+  }).sort({ createdAt: 1 }).exec();
+
+  return chatHistoryForCurrentUser;
+};
+
+const getChatHistoryForSelectedReceiver = async (userId) => {
+  const chatHistoryForSelectedReceiver = await Chat.find({
+    sender: userId,
+  }).sort({ createdAt: 1 }).exec();
+
+  return chatHistoryForSelectedReceiver;
+};
+
+const send = async (req, res) => {
   try {
     const { receiver, message } = req.body;
     const sender = req.user.id;
 
-    if (!receiver || !message) {
+    if (!sender || !message) {
       return res.status(400).json({ message: 'Receiver and message are required.' });
     }
 
-    const newChat = new Chat({ sender, receiver, message });
-    const savedChat = await newChat.save();
+    // Store the message for both users
+    await Chat.create({
+      sender,
+      receiver,
+      message,
+    });
 
-    res.status(201).json({ message: 'Message sent', data: savedChat });
+    res.status(201).json({ message: 'Message sent' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Server error', error: error.message });
   }
 };
 
-exports.receive = async (req, res) => {
+const receive = async (req, res) => {
   try {
     const { sender, message } = req.body;
     const receiver = req.user.id;
 
     if (!sender || !message) {
-      return res.status(400).json({ message: 'Sender and message are required.' });
+      return res.status(400).json({ message: 'Sender and message are required' });
     }
 
-    const newChat = new Chat({ sender, receiver, message });
-    const savedChat = await newChat.save();
+    // Store the message for both users
+    await Chat.create({
+      sender,
+      receiver,
+      message,
+    });
 
-    res.status(201).json({ message: 'Message received', data: savedChat });
+    // Send the message to the receiver
+    return res.status(201).json({ message: 'Message received' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Server error', error: error.message });
   }
 };
+
+module.exports = { getChatHistory, receive, send };
--- routers/ChatRoutes.js
+++ routers/ChatRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {send, receive, getChatHistory}= require('../controllers/ChatController');
+const { send, receive, getChatHistory } = require('../controllers/ChatController');
 const auth = require('./auth');
 
 // Retrieve chat messages for a specific user
@@ -9,3 +9,4 @@ router.get('/receive',auth, receive)
 router.post('/send',auth, send)
 
 module.exports = router;
+
